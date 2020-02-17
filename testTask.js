52056b2f397a93ca151c32fa9e5b17bc04f68a07
Alex-Dolid <dolsd.sasha@mail.ru>
2020-02-17 18:23:17 +0200

    Update testTask.js

--- testTask.js
+++ testTask.js
@@ -3,80 +3,57 @@
 (function () {
     "use strict";
 
-    // it is assumed that there is a playing field with the tag "table#field",
-    // which has the tag "tbody", which has the tags "tr", which has the tags "td[data-type-item].cell"
-    const tBody = document.querySelector("table > tbody");
-    const arrTr = Array.from(tBody.rows);
-
     function searchGameItems(event) {
         if (!event.target.closest("tr")) return;
 
+        // it is assumed that there is a playing field with the tag "table#field",
+        // which has the tag "tbody", which has the tags "tr", which has the tags "td[data-type-item].cell"
+        const tBody = document.querySelector("table > tbody");
+        const arrTr = Array.from(tBody.rows);
         const $ = event.target;
         const cells = document.querySelectorAll(".cell");
-        const currentGameItem = $.dataset.typeItem;
-        let allCellsWithTypeItem = [];
+        const currentGameTypeItem = $.dataset.typeItem;
+
+        //will be required to search on the top or bottom rows
         let setSearchCols = new Set();
 
-        currentGameItem.classList.add("selected");
+        // I denote the current cell
+        $.classList.add("selected");
+
+        for (let i = $; i.nextElementSibling; i = i.nextElementSibling) {
+            if(i.nextElementSibling.dataset.typeItem === currentGameTypeItem) {
+                i.nextElementSibling.classList.add("selected");
+            }
+        }
+
+        for (let i = $; i.previousElementSibling; i = i.previousElementSibling) {
+            if(i.previousElementSibling.dataset.typeItem === currentGameTypeItem) {
+                i.previousElementSibling.classList.add("selected");
+            }
+        }
+
+        // I find all cells with the same game character with class "selected" and add theirs cellIndexes
         cells.forEach(cell => {
-            if(cell.dataset.typeItem === currentGameItem) {
-                allCellsWithTypeItem.push(cell);
+            if(cell.dataset.typeItem === currentGameTypeItem && cell.classList.contains("selected")) {
+                setSearchCols.add(cell.cellIndex);
             }
         });
 
-
-        allCellsWithTypeItem.forEach(item => {
-            if(item === currentGameItem.nextElementSiblling || item === currentGameItem.previousElementSibling) {
-                item.classList.add("selected");
-            }
-        });
-
-        allCellsWithTypeItem.forEach(item => {
-           if(item.cellIndex === currentGameItem.cellIndex) {
-               if(!item.classList.contains("selected")) {
-                   if(item.nextElementSibling.classList.contains("selected") || item.previousElementSibling.classList.contains("selected")) {
-                       item.classList.add("selected");
-                   }
-               }
-           }
-        });
-
-        allCellsWithTypeItem.forEach((item, index) => {
-            if(item.classList.contains("selected")) {
-                setSearchCols.add(item.cellIndex);
-            }
-        });
-
+        // I notice rows, which which are verified
         arrTr.forEach((tr, index )=> {
-           if(index === currentGameItem.parentElement.rowIndex) {
+           if(index === $.parentElement.rowIndex) {
                tr.classList.add("noticed");
            }
         });
 
-        arrTr.forEach((tr, index) => {
-            if(tr === currentGameItem.parentElement.rowIndex - 1) {
-                let arrTd = Array.from(tr.cells);
-                arrTd.forEach((td, indx) => {
-                    if(setSearchCols.has(indx) && (td.dataset.typeItem === currentGameItem)) {
-                        td.classList.add("selected");
-                    }
-                })
-            } else if(tr === currentGameItem.parentElement.rowIndex + 1) {
-                let arrTd = Array.from(tr.cells);
-                arrTd.forEach((td, indx) => {
-                    if(setSearchCols.has(indx) && (td.dataset.typeItem === currentGameItem)) {
-                        td.classList.add("selected");
-                    }
-                })
-            }
-        });
-
+        // I find every row that is not marked and
+        // find every cell that is in the same position as the other checked cells in the noticed row
         while(arrTr.find(tr => !tr.classList.contains("noticed"))) {
             for (let i = 0; i < arrTr.length; i++) {
                 if(!arrTr[i].classList.contains("noticed") && arrTr[i-1].classList.contains("noticed")) {
                     let arrTd = Array.from(arrTr[i].cells);
                     arrTd.forEach((td, index) => {
-                        if(setSearchCols.has(index) && (td.dataset.typeItem === currentGameItem)) {
+                        if(setSearchCols.has(index) && (td.dataset.typeItem === currentGameTypeItem)) {
                             td.classList.add("selected");
                         }
                     });
@@ -84,7 +61,7 @@
                 } else if(!arrTr[i].classList.contains("noticed") && arrTr[i+1].classList.contains("noticed")) {
                     let arrTd = Array.from(arrTr[i].cells);
                     arrTd.forEach((td, index) => {
-                        if(setSearchCols.has(index) && (td.dataset.typeItem === currentGameItem)) {
+                        if(setSearchCols.has(index) && (td.dataset.typeItem === currentGameTypeItem)) {
                             td.classList.add("selected");
                         }
                     });
