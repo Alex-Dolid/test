37454e7fa2aab8aa40a10774510f030d363a82f1
Alex-Dolid <dolsd.sasha@mail.ru>
2020-02-17 15:47:50 +0200

    Update testTask.js

--- testTask.js
+++ testTask.js
@@ -3,44 +3,97 @@
 (function () {
     "use strict";
 
-    const field = document.querySelector(".field");
-    //it is assumed that there is a playing field with class "field"
-
+    // it is assumed that there is a playing field with the tag "table#field",
+    // which has the tag "tbody", which has the tags "tr", which has the tags "td[data-type-item].cell"
+    const tBody = document.querySelector("table > tbody");
+    const arrTr = Array.from(tBody.rows);
 
     function searchGameItems(event) {
-        if(!event.target.classList.contains("cell")) return;
+        if (!event.target.closest("tr")) return;
 
         const $ = event.target;
         const cells = document.querySelectorAll(".cell");
-        // it is assumed that there is a playing field with class "field" which contains blocks with class "cell"
-        // blocks with class "cell" have data-attributes type-item with values "<typeGameElement>"
         const currentGameItem = $.dataset.typeItem;
+        let allCellsWithTypeItem = [];
+        let setSearchCols = new Set();
 
+        currentGameItem.classList.add("selected");
         cells.forEach(cell => {
-
-            if(cell.dataset.typeItem === currentGameItem) return true;
-            // we've found all of the game elements by cell, and we can already erase them in the future
-
+            if(cell.dataset.typeItem === currentGameItem) {
+                allCellsWithTypeItem.push(cell);
+            }
         });
 
+
+        allCellsWithTypeItem.forEach(item => {
+            if(item === currentGameItem.nextElementSiblling || item === currentGameItem.previousElementSibling) {
+                item.classList.add("selected");
+            }
+        });
+
+        allCellsWithTypeItem.forEach(item => {
+           if(item.cellIndex === currentGameItem.cellIndex) {
+               if(!item.classList.contains("selected")) {
+                   if(item.nextElementSibling.classList.contains("selected") || item.previousElementSibling.classList.contains("selected")) {
+                       item.classList.add("selected");
+                   }
+               }
+           }
+        });
+
+        allCellsWithTypeItem.forEach((item, index) => {
+            if(item.classList.contains("selected")) {
+                setSearchCols.add(item.cellIndex);
+            }
+        });
+
+        arrTr.forEach((tr, index )=> {
+           if(index === currentGameItem.parentElement.rowIndex) {
+               tr.classList.add("noticed");
+           }
+        });
+
+        arrTr.forEach((tr, index) => {
+            if(tr === currentGameItem.parentElement.rowIndex - 1) {
+                let arrTd = Array.from(tr.cells);
+                arrTd.forEach((td, indx) => {
+                    if(setSearchCols.has(indx) && (td.dataset.typeItem === currentGameItem)) {
+                        td.classList.add("selected");
+                    }
+                })
+            } else if(tr === currentGameItem.parentElement.rowIndex + 1) {
+                let arrTd = Array.from(tr.cells);
+                arrTd.forEach((td, indx) => {
+                    if(setSearchCols.has(indx) && (td.dataset.typeItem === currentGameItem)) {
+                        td.classList.add("selected");
+                    }
+                })
+            }
+        });
+
+        while(arrTr.find(tr => !tr.classList.contains("noticed"))) {
+            for (let i = 0; i < arrTr.length; i++) {
+                if(!arrTr[i].classList.contains("noticed") && arrTr[i-1].classList.contains("noticed")) {
+                    let arrTd = Array.from(arrTr[i].cells);
+                    arrTd.forEach((td, index) => {
+                        if(setSearchCols.has(index) && (td.dataset.typeItem === currentGameItem)) {
+                            td.classList.add("selected");
+                        }
+                    });
+                    arrTr[i].classList.add("noticed");
+                } else if(!arrTr[i].classList.contains("noticed") && arrTr[i+1].classList.contains("noticed")) {
+                    let arrTd = Array.from(arrTr[i].cells);
+                    arrTd.forEach((td, index) => {
+                        if(setSearchCols.has(index) && (td.dataset.typeItem === currentGameItem)) {
+                            td.classList.add("selected");
+                        }
+                    });
+                    arrTr[i].classList.add("noticed");
+                }
+            }
+        }
     }
 
     field.addEventListener("click", searchGameItems); //
 
 })();
-
-
-
-/*
-* There also may be other search options, depending on situation, on which the search should be done.
-* For example, the interface of playing field can be released not on a data-attributes. Then in another case we need
-*  to make search with "id" or "img"
-* */
-
-
-/*
-* And this task can be set in the realities of React App. In such case we need to use the store in the architecture
-*  "Flux" or "Redux" and therefore to update the local state and if needed to synchronized this state to Server-Side
-*  witch the help of Fetch API
-*
-* */
